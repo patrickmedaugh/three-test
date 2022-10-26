fe19c3ca020c7e12e2e2b4d335d4927589e021b4
patrickmedaugh <patrickmedaugh@gmail.com>
2022-10-25 20:33:06 -0600

    Drawing lines tutorial

--- src/index.js
+++ src/index.js
@@ -1,36 +1,26 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import WebGL from './webgl';
-console.log('hi')
+// Blue triangle thing from: https://threejs.org/docs/index.html#manual/en/introduction/Drawing-lines
+import * as THREE from 'three'
 
-const renderer = new THREE.WebGLRenderer();
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const controls = new OrbitControls( camera, renderer.domElement );
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500)
+camera.position.set(0, 0, 100)
+camera.lookAt(0, 0, 0)
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const scene = new THREE.Scene()
 
-camera.position.z = 5;
+const material = new THREE.LineBasicMaterial( { color: 0x000ff })
 
-function animate() {
-    requestAnimationFrame(animate);
+const points = []
+points.push(new THREE.Vector3( -10, 0, 0))
+points.push(new THREE.Vector3( 0, 10, 0))
+points.push(new THREE.Vector3( 10, 0, 0))
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+const geometry = new THREE.BufferGeometry().setFromPoints(points)
 
-    renderer.render(scene, camera);
-}
+const line = new THREE.Line(geometry, material)
 
-if (WebGL.isWebGLAvailable()) {
-    animate();
-
-} else {
-    const warning = WebGL.getWebGLErrorMessage();
-    document.getElementById('container').appendChild(warning);
-}
+scene.add(line)
+renderer.render(scene, camera)
